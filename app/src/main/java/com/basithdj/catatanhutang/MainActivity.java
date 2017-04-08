fc775204bdee863c29f74016579abd264d6d7524
basith djunaedi <basithdjunaedi@gmail.com>
2017-04-08 14:42:55 +0700

    buat activity baru

--- app/src/main/java/com/basithdj/catatanhutang/MainActivity.java
+++ app/src/main/java/com/basithdj/catatanhutang/MainActivity.java
@@ -1,5 +1,6 @@
 package com.basithdj.catatanhutang;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.basithdj.catatanhutang.hutang.CreateHutangActivity;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,15 +25,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -97,5 +91,9 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void openCreateActivity (View view) {
+        startActivity(new Intent(this, CreateHutangActivity.class));
     }
 }
---
+++ app/src/main/java/com/basithdj/catatanhutang/hutang/CreateHutangActivity.java
@@ -0,0 +1,15 @@
+package com.basithdj.catatanhutang.hutang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.basithdj.catatanhutang.R;
+
+public class CreateHutangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_hutang);
+    }
+}
